c853dac3dab36734e0db349065e3288f8e89f5c5
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-10-07 11:14:36 -0700

    Seperated the main function file, header file, and function implementation file. Also, used namespace to make the code more consistent during function to call

---
+++ LinearSearchAlgorithm/search.cpp
@@ -0,0 +1,8 @@
+//
+//  search.cpp
+//  LinearSearchAlgorithm
+//
+//  Created by Chatchapong Thuaypha on 10/7/24.
+//
+
+#include <stdio.h>
---
+++ LinearSearchAlgorithm/search.h
@@ -0,0 +1,12 @@
+//
+//  search.h
+//  LinearSearchAlgorithm
+//
+//  Created by Chatchapong Thuaypha on 10/7/24.
+//
+
+#ifndef search_h
+#define search_h
+
+
+#endif /* search_h */
